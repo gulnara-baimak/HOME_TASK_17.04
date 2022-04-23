05336654311d8cf89129c701f0e67cc5eacb24eb
Gulnara <gulnara@mail.rum>
2022-04-23 23:43:27 +0600

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.  14 -> нет  46 -> нет  161 -> да
+
+int Number1 = new Random().Next(11, 101);
+int Number2 = new Random().Next(1, 11);
+int result = 0;
+{
+  result = Number1 % Number2;
+}
+System.Console.WriteLine();
+if (result == 0) System.Console.WriteLine($"Число {Number1} кратно числу {Number2}");
+else
+{
+    Console.WriteLine($"Число {Number1} не кратно числу {Number2}");
+}
+System.Console.WriteLine();
+
+            
+    
