1f821be1e354bd9ce0218df0e57e759c037c1f9b
Gulnara <gulnara@mail.rum>
2022-04-23 23:40:48 +0600

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a > 1000 ? a.ToString()[2] : "третей цифры нет"); 
+Console.ReadKey(true);
+{
+    if (a >= 100 && a <=1000)
+    { 
+    Console.WriteLine((a / 100) % 10);
+    int b = a / 100 / 10;
+    int c = b % 10;
+    Console.WriteLine(c);
+    Console.WriteLine($"Последней цифрой числа {a} является цифра {a % 10}");
+    } 
+}
+
